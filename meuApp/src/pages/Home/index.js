6c478888567c3a55e33586ea6c245acff9fcd275
dapaz-programador <hermesdapaz.programador@gmail.com>
2024-02-23 15:27:49 -0300

    comeco do zero

---
+++ meuApp/src/pages/Home/index.js
@@ -0,0 +1,23 @@
+
+import { StyleSheet, Text, View } from 'react-native'; 
+import Header from '../../components/Header';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Header/>
+      <Text> Pagina Home!</Text>
+      <Text> Pagina Home!</Text>
+      <Text> Pagina Home!</Text>
+      <Text> Pagina Home!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00ff89',
+    
+  },
+});
